cf70c5f0a8ae1defa32b1fbe505e9ebb80ca678e
Mona Lisa <kgaurav1998@outlook.com>
2022-07-19 14:35:47 +0530

    change

--- addstudent_db.php
+++ addstudent_db.php
@@ -2,21 +2,18 @@
 
 require "config/dbconfig.php";
 
-$student_id=$_POST["student_id"];
+
 $prn_no=$_POST["prn_no"];
 $student_name=$_POST["student_name"];
 $student_email=$_POST["student_email"];
 $password=$_POST["password"];
-$student_dept_=$_POST["student_dept"];
+$student_dept=$_POST["student_dept"];
 $student_mobile=$_POST["student_mobile"];
 
 
-if(!empty($admin_email) || !empty($admin_email)){
-
-$r="select * from admin where admin_email='$admin_email' AND password='$password'";
+$r="insert into student (prn_no, student_name,student_email,password,student_dept,student_mobile) values('$prn_no','$student_name','$student_email')";
 $result = mysqli_query($con,$r);
-$count=mysqli_num_rows($result);
-if($count>0){
+
     if($row=mysqli_fetch_array($result))
         header("location:starter.html");
     }
@@ -24,7 +21,7 @@ if($count>0){
        header("location:addstudent.php");
        die();
     
-}}
+}
 
 
 ?>
